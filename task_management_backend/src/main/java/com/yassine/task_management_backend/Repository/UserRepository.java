0ebb51cf943d68d8295b8b88b532b3d2bfed683c
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2024-07-10 15:58:26 +0100

    Added UserRepository interface for user data access
    - Extends JpaRepository<User, Long> to provide CRUD operations for User entities.
    - Includes findByEmail method for retrieving a user by email address.
    - Facilitates structured data access and management for user entities within the application.

--- task_management_backend/src/main/java/com/yassine/task_management_backend/Repository/UserRepository.java
+++ task_management_backend/src/main/java/com/yassine/task_management_backend/Repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.yassine.task_management_backend.Repository;
 import com.yassine.task_management_backend.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Repository interface for managing User entities
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Method to find a user by their email address
     User findByEmail(String email);
 
 }
