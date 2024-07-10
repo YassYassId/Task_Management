7c736db508b0563e35327e8b5f0e173477eac352
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2024-07-10 15:55:03 +0100

    Implemented Task entity for task management
    - Defined Task entity to represent tasks with attributes such as title, description, due date, and status.
    - Configured relationship with User entity for task assignment.
    - Supports mapping to a database table named "tasks" for data persistence.

--- task_management_backend/src/main/java/com/yassine/task_management_backend/Entity/Task.java
+++ task_management_backend/src/main/java/com/yassine/task_management_backend/Entity/Task.java
@@ -19,15 +19,19 @@ public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private String description;
-    private LocalDate dueDate;
+    private Long id; // Unique identifier for the task
+
+    private String title; // Title or name of the task
+
+    private String description; // Description or details of the task
+
+    private LocalDate dueDate; // Due date for completing the task
 
     @Enumerated(EnumType.STRING)
-    private Status status;
+    private Status status; // Current status of the task (e.g., pending, completed)
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private User user;
+    private User user; // User assigned to the task
+
 }
