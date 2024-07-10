535a50adb511caa516192c20e327bb93e4ef6514
Yassine IDRISSI <112396575+Yassid-tech@users.noreply.github.com>
2024-07-10 16:00:44 +0100

    Implemented TaskService for task management operations
    - Autowired TaskRepository for CRUD operations on Task entities.
    - Provides methods to retrieve tasks by user ID, retrieve a task by ID and user ID, create tasks, and delete tasks.
    - Facilitates structured management of tasks within the application.

--- task_management_backend/src/main/java/com/yassine/task_management_backend/Service/TaskService.java
+++ task_management_backend/src/main/java/com/yassine/task_management_backend/Service/TaskService.java
@@ -13,21 +13,26 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class TaskService {
+
     @Autowired
     private TaskRepository taskRepository;
 
+    // Retrieves all tasks associated with a specific user
     public List<Task> getAllTasksByUser(Long userId) {
         return taskRepository.findByUserId(userId);
     }
 
+    // Retrieves a task by its ID and associated user ID
     public Optional<Task> getTaskByIdAndUser(Long taskId, Long userId) {
         return taskRepository.findByIdAndUserId(taskId, userId);
     }
 
+    // Creates a new task in the database
     public Task createTask(Task task) {
         return taskRepository.save(task);
     }
 
+    // Deletes a task from the database by its ID
     public void deleteTask(Long taskId) {
         taskRepository.deleteById(taskId);
     }
